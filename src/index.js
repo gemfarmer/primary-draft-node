a42e14eca77eeb4b16188050fe26a38eaa32f39a
Brian Hedberg <bshslowfood@gmail.com>
2019-04-04 13:35:38 -0500

    break routes into routes/ dir

--- src/index.js
+++ src/index.js
@@ -2,19 +2,29 @@ import 'dotenv/config';
 console.log('Hello Node.js project.');
 console.log(process.env.MY_SECRET);
 import cors from 'cors';
+import bodyParser from 'body-parser';
 import express from 'express';
 
+import models from './models';
+import routes from './routes';
 const app = express();
 
 app.use(cors());
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  req.context = {
+    models,
+    me: models.users[1],
+  };
+  next();
 });
+app.use('/session', routes.session);
+app.use('/users', routes.user);
+app.use('/messages', routes.message);
 
-app.get('/test', (req, res) => {
-  res.send('Hello World! test test');
-});
+
+
 
 app.listen(process.env.PORT, () =>
   console.log(`Example app listening on port ${process.env.PORT}!`),
