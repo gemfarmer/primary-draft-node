579d9f85c494f6cd4f714512162153f6f402c532
Brian Hedberg <bshslowfood@gmail.com>
2019-04-04 14:14:45 -0500

    basic API

--- src/index.js
+++ src/index.js
@@ -5,17 +5,17 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import express from 'express';
 
-import models from './models';
+import models, { sequelize } from './models';
 import routes from './routes';
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use((req, res, next) => {
+app.use(async (req, res, next) => {
   req.context = {
     models,
-    me: models.users[1],
+    me: await models.User.findByLogin('brianhedberg'),
   };
   next();
 });
@@ -24,10 +24,47 @@ app.use('/users', routes.user);
 app.use('/messages', routes.message);
 
 
+const eraseDatabaseOnSync = false;
 
+sequelize.sync({ force: eraseDatabaseOnSync }).then(async () => {
+  if (eraseDatabaseOnSync) {
+    createUsersWithMessages();
+  }
 
-app.listen(process.env.PORT, () =>
-  console.log(`Example app listening on port ${process.env.PORT}!`),
-);
+  app.listen(process.env.PORT, () =>
+    console.log(`Example app listening on port ${process.env.PORT}!`),
+  );
+});
 
+const createUsersWithMessages = async () => {
+  await models.User.create(
+    {
+      username: 'rwieruch',
+      messages: [
+        {
+          text: 'Published the Road to learn React',
+        },
+      ],
+    },
+    {
+      include: [models.Message],
+    },
+  );
 
+  await models.User.create(
+    {
+      username: 'brianhedberg',
+      messages: [
+        {
+          text: 'Hello',
+        },
+        {
+          text: 'WOrld',
+        },
+      ],
+    },
+    {
+      include: [models.Message],
+    },
+  );
+};
